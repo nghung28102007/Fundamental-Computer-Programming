2ccdd79f2a6421ee694b134ff40b4cfc01507bcc
nghung28102007 <nghung28102007@gmail.com>
2026-01-09 10:22:58 +0700

    update lesson 2

--- lesson2.py
+++ lesson2.py
@@ -1,4 +1,10 @@
+#1
 user_hours = int(input("Enter hours: "))
 user_rate = float(input("Enter rate: "))
 gross_pay = user_hours * user_rate
 print("Gross pay: " + str(gross_pay))
+#2
+story_name = input("Enter your name: ")
+story_year = int(input("Enter your birth year: "))
+print(f"{story_name} is a vliant knight, borin in the year {story_year}. One morning, {story_name} woke up to an awful racket: a dragon was approaching the village. Only {story_name} could save the village's residents.")
+
